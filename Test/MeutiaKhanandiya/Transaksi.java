c1aebcca07300b93e4eeb47dd5c413881720809a
MeutiaKhanandiya <43591697+MeutiaKhanandiya@users.noreply.github.com>
2018-11-30 23:28:42 +0700

    Menambahkan komentar pada class Transaksi

--- Test/MeutiaKhanandiya/Transaksi.java
+++ Test/MeutiaKhanandiya/Transaksi.java
@@ -11,11 +11,14 @@ package MeutiaKhanandiya;
  */
 import java.util.* ;
 public class Transaksi {
-    
+    // variable code untuk menampilkan kode pembayaran minuman
     private final String kode ;
+    // variable untuk menampilkan item yang akan di beli
     private ArrayList<Item> items = new ArrayList<>();
+    // variable total untuk menampilkan pembelian minuman
     private float total ;
     
+    // menambahkan Constructor
     public Transaksi (String kode , ArrayList<item> items){
             this.kode = kode ;
             this.items = items ;
@@ -28,13 +31,18 @@ public class Transaksi {
         }
         this.total = total ;
     }
-    
+    // memasukkan output transaksi
     public String Pembayaran() {
         setTotal();
         String obt = "" ;
         obt += "Kode\t\t : " + this.kode +"\n" ;
         obt += "Daftar Belanja : \n" ;
-        
+        for (Item item : this.items){
+            obt += "\t" + item.getNama() + "(x" + item.getJumlah() + ") : " + item.getTotal() + "\n" ;
+        }
+        obt += "Total\t\t : " + this.total ;
+        return obt ;
+        }
     }
 
     
