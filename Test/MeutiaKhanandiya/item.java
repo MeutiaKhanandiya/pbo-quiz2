0d182ecba88b3e3aa95a8ebc5993bcd93b05a6b2
MeutiaKhanandiya <43591697+MeutiaKhanandiya@users.noreply.github.com>
2018-11-30 15:56:38 +0700

    Menambahkan Variable Nama, harga, jumlah pada class Item.

---
+++ Test/MeutiaKhanandiya/item.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MeutiaKhanandiya;
+
+/**
+ *
+ * @author MEUTIA KHANANDIYA
+ */
+public class item {
+     private String nama;
+     private float harga;
+     private int jumlah;
+     
+}
