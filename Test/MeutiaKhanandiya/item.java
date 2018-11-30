2781e1a20def04580a71cd1c0e7dd0a8ed71b969
MeutiaKhanandiya <43591697+MeutiaKhanandiya@users.noreply.github.com>
2018-11-30 16:37:48 +0700

    Menambahkan Constructor pada class Item beserta Getter Setter yang telah di tetapkan.

--- Test/MeutiaKhanandiya/item.java
+++ Test/MeutiaKhanandiya/item.java
@@ -14,4 +14,44 @@ public class item {
      private float harga;
      private int jumlah;
      
+     //menambahkan Constructor pada Class Item
+     public item(){
+     }
+     
+     
+    public item(String nama, float harga, int jumlah) {
+        //memberi tau nama barang yang tersedia
+        this.nama = nama;
+        //memberi tau harga yang telah di tetapkan
+        this.harga = harga;
+        //memberi tau jumlah barang yang telah di beli 
+        this.jumlah = jumlah;
+    }
+        // menambahkan Getter Setter Pada Class Item
+    
+    //Get Nama
+    public String getNama() {
+        return nama;
+    }
+    //set Nama
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    //Get Harga
+    public float getHarga() {
+        return harga;
+    }
+    //Set Harga
+    public void setHarga(float harga) {
+        this.harga = harga;
+    }
+    //Get Jumlah
+    public int getJumlah() {
+        return jumlah;
+    }
+    //Set Jumlah
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+        
+    }
 }
